11d8e373b4b6cbd9160d77d6b1c5613485a74ce9
JINISHAM <jinishamurugesh@gmail.com>
2024-01-21 20:52:23 +0530

    Javascript basics

---
+++ operators.js
@@ -0,0 +1,36 @@
+//Assignment Operator
+var a = 10
+console.log("Assignment Operator output:",a)
+//Arithmentic Operator
+var aA = 10
+var bA = 10
+console.log(typeof aA)          //To check the type of input
+console.log("Arithmetic Operator addition output:",aA+bA)
+console.log("Arithmetic Operator subtraction output:",aA-bA)
+console.log("Arithmetic Operator multiplication output:",aA*bA)
+console.log("Arithmetic Operator division output:",aA/bA)
+console.log("Arithmetic Operator modulus output:",aA%bA)
+//Logical Operator --> &&, ||, !
+var aL = 10
+var bL = 10
+console.log("Logical Operator && output:", aL&&bL)
+console.log("Logical Operator || output:", aL||bL)
+console.log("Logical Operator ! output:",! bL)
+
+console.log(10 && 20)
+console.log(0 && 20)
+console.log(10 && 0)
+console.log(-10 && 20)
+console.log(-10 && -20)
+
+console.log(10 || 20)
+console.log(0 || 20)
+console.log(10 || 0)
+console.log(-10 || 20)
+console.log(-10 || -20)
+
+console.log(!10)
+console.log(!0)
+console.log(!-10)
+
+
